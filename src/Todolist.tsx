d4f4947aa18f6c2c66e4f498c2d92c4eb046cc78
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-10-18 19:11:56 +0300

    -add repo-todolist-02

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,39 +1,32 @@
-import React from "react";
-import {Tasks} from "./components/Tasks";
-import {Students} from "./components/Students";
+import React from 'react';
 
-export type TaskType = {
-	taskId: number,
-	title: string,
-	isDone: boolean
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
-type DateListType = {
-	title: string,
-	tasks: TaskType[],
-	students: Array<string>
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
 }
 
-type TodolistPropsType = {
-	task: DateListType
-}
-
-export const Todolist = (props: TodolistPropsType) => {
-	return (
-		<div>
-			<h3>{props.task.title}</h3>
-			<div>
-				<input type="text"/>
-				<button>+</button>
-			</div>
-			<Tasks tasksList = {props.task.tasks}/>
-			<div>
-				<button>All</button>
-				<button>Active</button>
-				<button>Completed</button>
-			</div>
-			<h3>Students</h3>
-			<Students studentsList = {props.task.students}/>
-		</div>
-	)
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
 }
